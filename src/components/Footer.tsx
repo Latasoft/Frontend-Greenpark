29e2bcfadd2399bc5e68e0cb0a0d92e3c4fc16e0
DexusRandgris <j.choque@outlook.com>
2025-07-08 16:17:33 -0400

    Actualizacion-Footer
    Detalle con espacio y coma.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
                 <svg className="w-4 h-4 mr-2 mt-1" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
                 </svg>
-                <span>Los Damascos 132 Francke ,<br/>Osorno, Chile</span>
+                <span>Los Damascos 132 Francke,<br/>Osorno, Chile</span>
               </p>
             </div>
             
