594de883d44bca220d6509a111adecb29e37ec32
Latasoft <latasoftweb@gmail.com>
2025-08-25 19:42:06 -0400

    boton de basurero sobre por boton de "x"

--- src/User-Panel/components/UserCourses.tsx
+++ src/User-Panel/components/UserCourses.tsx
@@ -245,50 +245,15 @@ const UserCourses = () => {
               </div>
             </div>
 
-            <button
+            <button 
               onClick={() => handleDeleteCurso(course.id)}
               disabled={deletingCourseId === course.id}
-              className="p-4 text-red-600 hover:text-red-800 transition-colors"
+              className="text-red-500 hover:text-red-700 transition-colors focus:outline-none"
               title="Eliminar curso"
-              aria-label={`Eliminar curso ${course.nombre}`}
             >
-              {deletingCourseId === course.id ? (
-                <svg
-                  className="animate-spin h-5 w-5 text-red-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                  ></path>
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              )}
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 112 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
+              </svg>
             </button>
           </li>
         ))}
