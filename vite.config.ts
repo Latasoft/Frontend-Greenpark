b3dbb3a09c003bae8899ea9465e6ca30c62ddb06
Latasoft <latasoftweb@gmail.com>
2025-08-26 03:19:11 -0400

    package

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/',
-  esbuild: {
-    logOverride: {
-      'this-is-undefined-in-esm': 'silent'
-    }
-  }
+  plugins: [react()],
 })
 
 
